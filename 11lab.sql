8a90a41a3646d1fbcfdecf3287f81862012c2f98
Maks <maximgaber65@gmail.com>
2020-11-03 23:13:53 +0200

    11lab

---
+++ 11lab.sql
@@ -0,0 +1,8 @@
+
+
+BEGIN TRANSACTION 
+INSERT INTO Film VALUES (8,'Gangs of New York','2002-1-10','Martin','Scorcese','','Action',169);
+INSERT INTO Film VALUES (9,'Shindler`s list','1993-10-11','Steven','Spilberg','','Drama',195);
+INSERT INTO Film VALUES (10,'Cloud Atlas','2012-09-11','Lana','Vachovski','','Science fiction',172);
+INSERT INTO Film VALUES (11,'Matrix','1999-03-10','Lana','Vachovski','','Science fiction',136);
+ROLLBACK
