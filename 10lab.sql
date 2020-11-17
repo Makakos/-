47fd6690e4af1a8bc72ab6e159bf75ee46126729
Maks <maximgaber65@gmail.com>
2020-11-17 15:44:14 +0200

    10lab

---
+++ 10lab.sql
@@ -0,0 +1,42 @@
+USE Movie_rental
+/*1*/
+CREATE FUNCTION Encription (@Film_name char(50))
+RETURNS char(50)
+BEGIN  
+    DECLARE @Result char(50)  
+    SET @Result = EncryptByPassPhrase('12',@Film_name) 
+    RETURN @Result  
+END
+
+
+
+CREATE FUNCTION Decription (@Value_for_decription char(50))
+RETURNS char(50)
+BEGIN  
+    DECLARE @Film_name char(50)  
+    SET @Film_name = DecryptByPassPhrase('12',@Value_for_decription)
+    RETURN @Film_name  
+END
+
+/*2*/
+CREATE PROCEDURE Film_studio_films (@date1 datetime,@date2 datetime)
+AS
+BEGIN
+    DECLARE @error CHAR;
+	SET @error = 'Некоректно задані дати';
+	IF (@date1<=@date2) 
+	BEGIN
+		SELECT  COUNT(DISTINCT f.Id_film) as Number_of_films,fs.Studio_name as Film_studio,@date1 as 'From', @date2 as 'To' 
+		FROM Film f INNER JOIN Movie_rental_order ON f.Id_film=Movie_rental_order.Id_film
+		INNER JOIN Film_studio fs ON fs.Id_film_studio=Movie_rental_order.Id_film_studio
+		WHERE f.Release_date BETWEEN @date1 and @date2
+		GROUP BY fs.Studio_name;
+	END;
+	ELSE SELECT @error;
+END;
+
+
+
+EXEC Film_studio_films'1980-05-05', '2015-01-01'; 
+
+
